5d5e12664acaddcc53f3ad7d0e2e71d6ebcc7d16
Ursu Adrian <uursu.adrian@gmail.com>
2022-10-24 20:57:10 +0200

    finished section22 - adding auth (firebase)

--- Section22 - Adding Authentication to React Apps/src/components/Auth/AuthForm.js
+++ Section22 - Adding Authentication to React Apps/src/components/Auth/AuthForm.js
@@ -1,10 +1,15 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useContext } from "react";
+import AuthContext from "../../store/auth-context";
+import { useHistory } from "react-router-dom";
 
 import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
+  const history = useHistory();
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+
+  const authCtx = useContext(AuthContext);
 
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
@@ -22,36 +27,52 @@ const AuthForm = () => {
     // optional: Add validation
 
     setIsLoading(true);
+    let url;
+
     if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC8Asc1aCWB2EtwNgk22la6mzvSg-bNih0";
     } else {
-      fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC8Asc1aCWB2EtwNgk22la6mzvSg-bNih0",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      ).then((res) => {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC8Asc1aCWB2EtwNgk22la6mzvSg-bNih0";
+    }
+
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
         setIsLoading(false);
         if (res.ok) {
-          //
+          return res.json();
         } else {
           return res.json().then((data) => {
             let errorMessage = "Authentication failed!";
-            if (data && data.error && data.error.message) {
-              errorMessage = data.error.message;
+            if (data?.error?.message) {
+              errorMessage = data?.error?.message;
             }
-            alert(errorMessage);
+
+            throw new Error(errorMessage);
           });
         }
+      })
+      .then((data) => {
+        const expirationTime = new Date(
+          new Date().getTime() + +data.expiresIn * 1000
+        );
+        authCtx.login(data.idToken, expirationTime.toString());
+        history.replace("/");
+      })
+      .catch((err) => {
+        alert(err.message);
       });
-    }
   };
 
   return (
--- Section22 - Adding Authentication to React Apps/src/components/Profile/ProfileForm.js
+++ Section22 - Adding Authentication to React Apps/src/components/Profile/ProfileForm.js
@@ -1,17 +1,52 @@
-import classes from './ProfileForm.module.css';
+import { useRef, useContext } from "react";
+import AuthContext from "../../store/auth-context";
+import classes from "./ProfileForm.module.css";
+import { useHistory } from "react-router-dom";
 
 const ProfileForm = () => {
+  const newPasswordInputRef = useRef();
+  const history = useHistory();
+
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const enteredNewPassword = newPasswordInputRef.current.value;
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyC8Asc1aCWB2EtwNgk22la6mzvSg-bNih0",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: authCtx.token,
+          password: enteredNewPassword,
+          returnSecureToken: false,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((res) => {
+      // asumption: always succeeds!
+      history.replace("/");
+    });
+  };
   return (
     <form className={classes.form}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input
+          type="password"
+          id="new-password"
+          minLength="6"
+          ref={newPasswordInputRef}
+        />
       </div>
       <div className={classes.action}>
-        <button>Change Password</button>
+        <button onClick={submitHandler}>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
